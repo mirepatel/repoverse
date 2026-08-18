a55020491415c7c796f599b7b4eda58931d97178
Mire Patel <mirepatel@gmail.com>
2026-08-18 14:12:51 +0100

    feat: add initial 3d scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function Cube() {
+  return (
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black">
-      <Button>
-        Explore Repoverse
-      </Button>
+    <main className="h-screen w-full bg-black">
+      <Canvas camera={{ position: [3, 3, 5], fov: 50 }}>
+        <ambientLight intensity={1} />
+
+        <directionalLight
+          position={[5, 5, 5]}
+          intensity={2}
+        />
+
+        <Cube />
+
+        <OrbitControls />
+      </Canvas>
     </main>
   )
 }
